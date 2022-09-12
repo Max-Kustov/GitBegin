5026f942d8c3f117622812b9e8928d6e04c3c00a
Максим Кустов <maksimka.kustov@yandex.ru>
2022-09-12 15:33:10 +0300

    Переработан способ ввода и обработки данных. Просчет из целой строки.

--- GitBegin/Program.cs
+++ GitBegin/Program.cs
@@ -14,25 +14,42 @@ namespace Calculator
             while (true)
             {
 
+                string firstStringValue = null;
+                string secondStringValue = null;        // промежуточные элементы разложеные из пользлвательской строки на отдельные элементы
+                string simbolS = null;                  // перенес в тело цикла чтобы при каждой итерации значения сбрасывались
+
+
                 Console.Clear();
                 Console.WriteLine("\t\tКалькулятор");
                 Console.WriteLine("Итерация " + counter);
-                try
+
+
+
+                Console.WriteLine("Введите выражение");
+                string expression = Console.ReadLine();
+
+
+                for (int i = 0; i < expression.Length; i++)                 // первый цикл заполняет промежуточную строку до математического знака
                 {
-                    Console.WriteLine("Введите первое число");
-                    firstValue = double.Parse(Console.ReadLine());
-                    Console.WriteLine("Введите второе число");
-                    secondValue = double.Parse(Console.ReadLine());
+                    if (expression[i] == '/' || expression[i] == '*' || expression[i] == '-' || expression[i] == '+')   // нахождение в теле строки мат. знака и использование его как разделителя
+                    {
+                        simbolS = Convert.ToString((char)expression[i]);    // присвоение знака к своей переменной
+
+                        for (int k = i + 1; k < expression.Length; k++)       //вложеный цикл стартует с элемента последующего знаку и заполняет оставнимися элементами вторую строку
+                        {
+
+                            secondStringValue += Convert.ToString(expression[k]);
+
+                        }
+                        break;
+                    }
+                    firstStringValue += Convert.ToString(expression[i]);    // присвоение строке её значенияпосле if, для того чтобы не было исключения на момент парсинга
 
                 }
-                catch (Exception)
-                {
-                    Console.WriteLine("Неверный формат ввода данных");
-                    Console.ReadLine();
-                    continue;
-                }
-                Console.WriteLine("Введите символоперации ( + , - , / или * )");
-                simbol = char.Parse(Console.ReadLine());
+                simbol = Convert.ToChar(simbolS);
+                firstValue = double.Parse(firstStringValue);                // конвертация промежуточных строк в свои типы данных, с которыми будем работать
+                secondValue = double.Parse(secondStringValue);
+
 
                 switch (simbol)
                 {
