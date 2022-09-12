1710930d41c1998df8b4f65d4aaa93d8f3537ae7
user <user@WIN-SBP6O1O5K39>
2022-09-12 14:23:58 +0300

    Добавьте файлы проекта.

---
+++ GitBegin/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace GitBegin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Первая пробная версия");
+            Console.ReadLine();
+        }
+    }
+}
